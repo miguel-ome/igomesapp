04a8b88627e09c7a7eea10ed3cc89a7bd03cee1f
Eliseu Miguel <libmigueldev@hotmail.com>
2025-04-05 19:53:42 -0300

    Create test sign in with return access token

---
+++ src/app/useCase/Auth/signIn.useCase.spec.ts
@@ -0,0 +1,30 @@
+import { User } from '@app/entities/User/User';
+import { SignInUseCase } from './signIn.useCase';
+import { JWTService } from '@app/auth/JWTService';
+
+describe('Sign in', () => {
+  let signInUseCase: SignInUseCase;
+  let jwtService: JWTService;
+
+  beforeEach(() => {});
+
+  it('Should be able sign and receipt access token', () => {
+    jwtService = {
+      genereteToken: jest.fn().mockReturnValue('mocked-token'),
+      verifyToken: jest.fn(),
+    };
+    signInUseCase = new SignInUseCase(jwtService);
+
+    const user = new User({
+      login: 'eliseu',
+      name: 'Eliseu Miguel',
+      password: 'teste123',
+    });
+
+    expect(signInUseCase.execute({ user })).toEqual(
+      expect.objectContaining({
+        access_token: 'mocked-token',
+      }),
+    );
+  });
+});
